97c5ada7f14d69b5cb936ce40b40ee168e219d60
DmitryYuGolubkov <dimma_i@mail.ru>
2018-11-20 17:57:48 +0300

    lock-free stack eaxmple listing 7.9

---
+++ cpp/cpp-concurrency-in-action/lock-free-stack/lock_free_stack_v7.9.cpp
@@ -0,0 +1,33 @@
+// implementation of lock-free stack for platforms with lock-free std::shared_ptr, where 
+// std::atomic_is_lock_free(&some_shared_ptr) == true
+
+template<typename T>
+class lock_free_stack
+{
+privte:
+  struct node
+  {
+    std::shared_ptr<T> data;
+    std::shared_ptr<node> next;
+    node(T const& data_) :
+      data( std::make_shared<T>( data_) )
+    {}
+  };
+  
+  std::shared_ptr<node> head;
+
+public:
+  void push(T const& data)
+  {
+    std::shared_ptr<node> const new_node = std::make_shared<node>( data );
+    new_node->next = head.load();  // if std::shared_ptr is atomic, it might have load()
+    while( !std::atomic_vompare_exchange_weak(&head, &new_node->next, new_node);
+  }
+  
+  std::shared_ptr<T> pop()
+  {
+    std::shared_ptr<node> old_head = std::atomic_load( &head );
+    while( old_head && !std::atomic_compare_exchange_weak(&head, &old_head, old_head->next) );
+    return old_head ? old_head->data : std::shared_ptr<T>();
+  }
+};
